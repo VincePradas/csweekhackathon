a321f4f7e76f699076765a29698f462de3d2ecd0
JakeNLelis <jake.lelis05@gmail.com>
2025-02-06 14:17:35 -0800

    sassa

--- main/src/App.js
+++ main/src/App.js
@@ -3,20 +3,16 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import './App.css';
 import Dashboard from './pages/dashboard/main';
 import List from './pages/product/list';
+import ProductPage from './components/ProductPage';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
-        <Route path="/admin/dashboard" element={<Dashboard />} />
-        <Route path="/admin/products" element={<List />} />
-        <Route path="/" element={<Navigate to="/admin/dashboard" replace />} />
-=======
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products" element={<List />} />
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
->>>>>>> 1bb54dcb11e87e740b386c628111543f9d070c77
       </Routes>
     </Router>
   );
